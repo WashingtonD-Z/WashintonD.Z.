04035170e1b9a21d9ef25bec7633cd8b069bc8c1
AsterosTheGreat <sylvestersnijders007@gmail.com>
2023-10-11 12:50:18 +0200

    Worked on the inventory system some more

--- Assets/Scripts/SurvivalSystem/Inventory.cs
+++ Assets/Scripts/SurvivalSystem/Inventory.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
 
-    public Canvas canvas;
+    public Canvas inventoryScreen;
+    public GameObject slotHolder;
+
+    private int slotCount;
+    private Transform[] slots;
+    
     private bool isOpened;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slotCount = slotHolder.transform.childCount;
+
+        for (int i = 0; i < slotCount; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i);
+        }
     }
 
     // Update is called once per frame
@@ -21,15 +31,21 @@ public class Inventory : MonoBehaviour
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true;
             isOpened = true;
-            canvas.enabled = true;
+            inventoryScreen.enabled = true;
         }
         else if (isOpened && Input.GetKeyDown(KeyCode.I))
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
             isOpened = false;
-            canvas.enabled = false;
+            inventoryScreen.enabled = false;
         }
 
     }
+
+
+    public void pickupItem()
+    {
+
+    } 
 }
