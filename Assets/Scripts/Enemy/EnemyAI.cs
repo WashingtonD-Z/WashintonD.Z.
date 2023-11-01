51504d2b3eb016fdd9e60ee11eeea8c4fd9fb64d
Tdiddy020105 <thomas.dielesen.gg@gmail.com>
2023-11-01 13:43:14 +0100

    Revert "Merge pull request #18 from WashingtonD-Z/WorldBuilding"
    This reverts commit 5de6931c36482cc7c9ae38ee7e72cbdf32f8155d, reversing
    changes made to 9301e827ad8a653e5bc167363cb6b0f4150e9583.

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public EnemyHealth enemyHealth;
+    public EnemyMovement movement;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("FPS Parkour Setup").transform;
+        agent.GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        enemyHealth.OnDeath += Die;
+    }
+    private void Update()
+    {
+        //Check  for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(!playerInSightRange && !playerInAttackRange) Patrolling();
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+        private void Patrolling()
+        {
+            if(!walkPointSet) SearchWalkPoint();
+
+            if(walkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+            }
+
+            Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+            //Walkpoint Reached
+            if(distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+        }
+
+        private void SearchWalkPoint()
+        {
+            float randomZ = Random.Range(-walkPointRange, walkPointRange);
+            float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+            walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+            if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            {
+                walkPointSet = true;
+            }
+        }
+
+        private void ChasePlayer()
+        {
+            agent.SetDestination(player.position);
+        }
+
+        private void AttackPlayer()
+        {
+            //Make sure enemy doesn't move
+            agent.SetDestination(transform.position);
+
+            transform.LookAt(player);
+            Debug.Log("Attacked");
+
+            if(!alreadyAttacked)
+            {
+
+                alreadyAttacked = true;
+                Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            }
+        }
+
+        private void ResetAttack()
+        {
+            alreadyAttacked = false;
+        }
+
+        private void Die(Vector3 position)
+        {
+            Debug.Log("Enemy has died");
+            movement.moveSpeed = 0;
+            movement.enabled = false;
+            this.enabled = false;
+        }
+}
